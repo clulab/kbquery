2f1d6724a59afd36598390d38f205d3ec6e396e9
Tom Hicks <hickst@tohono.com>
2017-04-08 21:12:39 -0700

    Change DB default text fields to varchar.

--- src/main/resources/sql/createTables.sql
+++ src/main/resources/sql/createTables.sql
@@ -2,9 +2,9 @@
 --
 CREATE TABLE `SOURCES` (
   `uid` int(11) NOT NULL,
-  `namespace` text NOT NULL,
-  `filename` text NOT NULL,
-  `label` text NOT NULL,
+  `namespace` varchar(20) NOT NULL,
+  `filename` varchar(60) NOT NULL,
+  `label` varchar(40) NOT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -13,18 +13,21 @@ CREATE TABLE `SOURCES` (
 --
 CREATE TABLE `ENTRIES` (
   `uid` INT NOT NULL AUTO_INCREMENT,
-  `text` text NOT NULL,
-  `namespace` text NOT NULL,
-  `id` text  NOT NULL,
-  `label` text  NOT NULL,
+  `text` varchar(80) NOT NULL,
+  `namespace` varchar(20) NOT NULL,
+  `id` varchar(20)  NOT NULL,
+  `label` varchar(40)  NOT NULL,
   `is_gene_name` TINYINT(1) NOT NULL,
   `is_short_name` TINYINT(1) NOT NULL,
-  `species` text NOT NULL,
+  `species` varchar(60) NOT NULL,
   `priority` INT NOT NULL,
   `source_ndx` INT NOT NULL,
   PRIMARY KEY (uid),
+  INDEX `text_ndx` (`text`),
+  INDEX `namespace_ndx` (`namespace`),
+  INDEX `id_ndx` (`id`),
+  INDEX `label_ndx` (`label`),
   KEY `SRC_FK`(`source_ndx`),
   CONSTRAINT SRC_FK FOREIGN KEY(`source_ndx`) REFERENCES `SOURCES`(`uid`)
     ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
